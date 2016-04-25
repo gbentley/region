7d5f03de780c55c081f67d1817470d5a318d68eb
Geoff Bentley <geoff@philandteds.com>
2016-04-26 10:11:34 +1200

    added region alert spanish translation

--- translations/esl-ES/translation.ts
+++ translations/esl-ES/translation.ts
@@ -1,19 +1,19 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0">
-<context>
-    <name>region</name>
-    <message>
-        <source>Select your region</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Go to our %region site</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Continue to our %region site</source>
-        <translation type="unfinished"></translation>
-    </message>
-</context>
+<TS>
+    <context>
+        <name>region</name>
+        <message>
+            <source>Continue to our %region site</source>
+            <translation>Continuar a nuestra página de %region</translation>
+        </message>
+        <message>
+            <source>Go to our %region site</source>
+            <translation>Ir a nuestra página de %region</translation>
+        </message>
+        <message>
+            <source>Select your region</source>
+            <translation>Selecciona tu región</translation>
+        </message>
+    </context>
 </TS>
