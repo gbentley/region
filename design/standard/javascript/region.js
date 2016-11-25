7476fd4b561dad4a733c339d6248f282a41b38e8
Mark Howard <mark@clearfield.com>
2016-11-25 15:37:27 +1300

    Minor tweaks: cleaned up the JSON output of region/check Some debugging info added to region JS

--- design/standard/javascript/region.js
+++ design/standard/javascript/region.js
@@ -1,36 +1,40 @@
-// MAIN MENU TOGGLES
+// Query current region via AJAX
 $(document).ready(function() {
+
+    console.log("Initiating /region/check");
 
     $.ajax({
         url: "/region/check",
-        cache: false
+        cache: false,
+        method: 'POST',
+        dataType: 'json'
     })
-    .done( function(result) {
-        if(result != '') {
-            //console.log("Site needs redirection");
+        .done( function(result) {
+            console.log("/region/check AJAX returned. Result: " + JSON.stringify(result));
 
-            var response = result;
-            if (typeof response == 'object') {
-                //console.log("Redirect URL = " + response.redirectto);                
-                $(document).on('click touchstart','.ui-dialog-buttonset button:first',function(e){
-                    e.preventDefault();
-                    location.href = response.redirectto;
-                    $("#dialog-confirm").dialog("close");
-                });
-                $(document).on('click touchstart','.ui-dialog-buttonset button:eq(1)',function(e){
-                    e.preventDefault();
-                    $("#dialog-confirm").dialog("close");
-                });
+            if(result != null) {
+                var response = result;
+                if (typeof response == 'object') {
 
-                $("#dialog-confirm").dialog({
-                    resizable: false,
-                    height: 340,
-                    width: 400,
-                    modal: true,
-                    buttons: [ { text: geo_go }, { text: geo_continue } ]
-                });
+                    $(document).on('click touchstart','.ui-dialog-buttonset button:first',function(e){
+                        e.preventDefault();
+                        location.href = response.redirectto;
+                        $("#dialog-confirm").dialog("close");
+                    });
+                    $(document).on('click touchstart','.ui-dialog-buttonset button:eq(1)',function(e){
+                        e.preventDefault();
+                        $("#dialog-confirm").dialog("close");
+                    });
+
+                    $("#dialog-confirm").dialog({
+                        resizable: false,
+                        height: 340,
+                        width: 400,
+                        modal: true,
+                        buttons: [ { text: geo_go }, { text: geo_continue } ]
+                    });
+                }
             }
-        }
-    });
+        });
 
 });
--- modules/region/regioncheck.php
+++ modules/region/regioncheck.php
@@ -3,17 +3,21 @@
 $module = $Params['Module'];
 
 if(!array_key_exists( 'REGIONCHECKED', $_COOKIE )) {
+    $result = array();
+
     //Check for session variable
     if(eZSession::issetkey('REGIONWARNING')) {
         $redirectURL = eZSession::get('SYSTEMIDENTIFIEDURL');
         $usURL = eZSession::get('USURL');
         $result = array(
-            'redirectto' => $redirectURL            
+            'redirectto' => $redirectURL
         );
-        $resultJson = json_encode($result);
-	header('Content-Type: application/json');
-        print_r($resultJson);
     }
     setcookie('REGIONCHECKED', 'TRUE', time()+3600*24*365 , '/' );
 }
+
+header('Content-Type: application/json');
+$resultJson = json_encode($result);
+print_r($resultJson);
+
 eZExecution::cleanExit();
